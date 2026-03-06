e981c0c07596b46ab0e8765e1aa58d5776f6f33d
KarllWest <karlwest396@gmail.com>
2026-03-06 18:23:52 +0000

    Small changes in API files

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,9 +1,17 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
+function getBaseURL() {
+  if (process.env.NEXT_PUBLIC_API_URL) {
+    return process.env.NEXT_PUBLIC_API_URL + "/api";
+  }
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}/api`;
+  }
+  return "http://localhost:3000/api";
+}
 
 const axiosInstance = axios.create({
-  baseURL,
+  baseURL: getBaseURL(),
   withCredentials: true,
 });
 
